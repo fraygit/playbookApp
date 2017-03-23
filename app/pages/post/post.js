5b0014558261ac89cd96461b6046313bb7097bdc
fray <francis.yanga@gmail.com>
2017-03-23 23:51:39 +1300

    post layout

--- app/pages/post/post.js
+++ app/pages/post/post.js
@@ -1,8 +1,21 @@
 ﻿var BasePage = require("../../shared/BasePage");
+var topmost = require("ui/frame").topmost;
 
 var PostPage = function () { };
 PostPage.prototype = new BasePage();
 PostPage.prototype.constructor = PostPage;
+
+PostPage.prototype.GoBack = function () {
+    topmost().navigate({
+        moduleName: "pages/home/home",
+        animated: true,
+        transition: {
+            name: "slide",
+            duration: 380,
+            curve: "easeIn"
+        }
+    });
+};
 
 
 module.exports = new PostPage();
