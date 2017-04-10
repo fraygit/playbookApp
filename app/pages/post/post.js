fc5bfffd56f5d68e975ae2c52ec7526ea0e41ae2
fray <francis.yanga@gmail.com>
2017-04-10 23:29:46 +1200

    image asset and image source

--- app/pages/post/post.js
+++ app/pages/post/post.js
@@ -8,6 +8,8 @@ var ObservableArray = require('data/observable-array');
 var imagepickerModule = require("nativescript-imagepicker");
 var http = require("http");
 var dialogs = require("ui/dialogs");
+var fs = require("file-system");
+var imageSource = require("image-source")
 
 var PostPage = function (args) {
     //var page = args.object;
@@ -72,7 +74,7 @@ var ReloadImages = function () {
     if (numOfImages > 0) {
         for (var i = 0; i < capturedImages.length; i++) {
             var newImage = new image.Image();
-            newImage.src = capturedImages[i].Image;
+            newImage.src = capturedImages[i].ImagePath;
             newImage.id = capturedImages[i].Id;
             newImage.class = "imagecaptured";
             newImage.height = imgWidth + '%';
@@ -89,6 +91,65 @@ var ReloadImages = function () {
     }
     
 };
+
+
+
+PostPage.prototype.OpenCamera = function () {
+    camera.requestPermissions();
+    camera.takePicture()
+        .then(function (imageAsset) {
+            var img = imageAsset
+            var savepath = fs.knownFolders.documents().path;
+            var filename = 'img_' + new Date().getTime() + '.jpg';
+            var filepath = fs.path.join(savepath, filename);
+
+            imageSource.fromAsset(imageAsset).then(function (res) {
+                console.log("from asset");
+                res.saveToFile(filepath, "jpg");
+
+                var newImageCaptured = { Id: generateGuid(), Image: imageAsset, ImagePath: filepath };
+                capturedImages.push(newImageCaptured);
+                console.log("image captured:" + filepath);
+                console.log(imageAsset)
+
+                ReloadImages();
+            });
+
+
+
+        }).catch(function (err) {
+            console.log("Error -> " + err.message);
+        });
+}
+
+
+PostPage.prototype.OpenGallery = function () {
+    var context = imagepickerModule.create({
+        mode: "multiple"
+    });
+
+    context
+        .authorize()
+        .then(function () {
+            return context.present();
+        })
+        .then(function (selection) {
+            console.log("Selection done:");
+            selection.forEach(function (selected) {
+                console.log(" - " + selected.uri);
+                var newImageCaptured = { Id: generateGuid(), ImagePath: selected.fileUri, Thumb: selected.thumb };
+                selectedImages.push(newImageCaptured);
+
+                ReloadImages();
+            });
+        }).catch(function (e) {
+            console.log(e);
+        });
+}
+
+var UploadMedia = function () {
+
+}
 
 PostPage.prototype.Post = function () {
     txtStory = page.getViewById("txtStory");
@@ -122,47 +183,6 @@ PostPage.prototype.Post = function () {
     });
 
 };
-
-PostPage.prototype.OpenCamera = function () {
-    camera.requestPermissions();
-    camera.takePicture()
-        .then(function (imageAsset) {
-            var newImageCaptured = { Id: generateGuid(), Image: imageAsset };
-            capturedImages.push(newImageCaptured);
-            console.log("image captured");
-            console.log(imageAsset)
-
-            ReloadImages();
-
-        }).catch(function (err) {
-            console.log("Error -> " + err.message);
-        });
-}
-
-
-PostPage.prototype.OpenGallery = function () {
-    var context = imagepickerModule.create({
-        mode: "multiple"
-    });
-
-    context
-        .authorize()
-        .then(function () {
-            return context.present();
-        })
-        .then(function (selection) {
-            console.log("Selection done:");
-            selection.forEach(function (selected) {
-                console.log(" - " + selected.uri);
-                var newImageCaptured = { Id: generateGuid(), ImagePath: selected.fileUri, Thumb: selected.thumb };
-                selectedImages.push(newImageCaptured);
-
-                ReloadImages();
-            });
-        }).catch(function (e) {
-            console.log(e);
-        });
-}
 
 
 module.exports = new PostPage();
