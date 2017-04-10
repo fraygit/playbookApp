f68c91d2118cee7c1837f1bf54caae86e85e9355
fray <francis.yanga@gmail.com>
2017-04-10 12:34:29 +1200

    add image gallery picker and modified post to handle choose gallery

--- app/pages/post/post.js
+++ app/pages/post/post.js
@@ -5,6 +5,7 @@ var label = require("ui/label");
 var image = require("ui/image");
 var Observable = require('data/observable');
 var ObservableArray = require('data/observable-array');
+var imagepickerModule = require("nativescript-imagepicker");
 
 var PostPage = function (args) {
     //var page = args.object;
@@ -48,44 +49,84 @@ function generateGuid() {
       s4() + '-' + s4() + s4() + s4();
 }
 
+var selectedImages = [];
 var capturedImages = [];
+
+var ReloadImages = function () {
+
+    pnlMedia = page.getViewById("pnlMedia");
+    txtStory = page.getViewById("txtStory");
+    txtStory.height = "50%";
+    pnlMedia.height = "42%";
+
+    pnlMedia.removeChildren();
+    //console.log("display images");
+    //console.log("selected images : " + selectedImages.length);
+
+    var numOfImages = capturedImages.length + selectedImages.length;
+    var imgWidth = 100 / numOfImages;
+    console.log("num images : " + numOfImages);
+
+    if (numOfImages > 0) {
+        for (var i = 0; i < capturedImages.length; i++) {
+            var newImage = new image.Image();
+            newImage.src = capturedImages[i].Image;
+            newImage.id = capturedImages[i].Id;
+            newImage.class = "imagecaptured";
+            newImage.height = imgWidth + '%';
+            pnlMedia.addChild(newImage);
+        }
+        for (var i = 0; i < selectedImages.length; i++) {
+            var newImage = new image.Image();
+            newImage.src = selectedImages[i].ImagePath;
+            newImage.id = selectedImages[i].Id;
+            newImage.class = "imagecaptured";
+            newImage.height = imgWidth + '%';
+            pnlMedia.addChild(newImage);
+        }
+    }
+    
+};
+
 
 PostPage.prototype.OpenCamera = function () {
     camera.requestPermissions();
     camera.takePicture()
         .then(function (imageAsset) {
-            txtStory = page.getViewById("txtStory");
-            pnlMedia = page.getViewById("pnlMedia");
-            txtStory.height = "50%";
-            pnlMedia.height = "42%";
-
-            //if (capturedImages.length > 0) {
-            //    for (var i = 0; i < capturedImages.length; i++) {
-            //        var displayedImage = page.getViewById(capturedImages[i].Id);
-            //        pnlMedia.removeChild(displayedImage);
-            //    }
-            //}
-            
-            pnlMedia.removeChildren();
-
             var newImageCaptured = { Id: generateGuid(), Image: imageAsset };
             capturedImages.push(newImageCaptured);
+            console.log("image captured");
+            console.log(imageAsset)
 
-            if (capturedImages.length > 0) { 
-                var imgWidth = 100 / capturedImages.length;
-                for (var i = 0; i < capturedImages.length; i++) {
-                    var newImage = new image.Image();
-                    newImage.src = capturedImages[i].Image;
-                    newImage.id = capturedImages[i].Id;
-                    newImage.class = "imagecaptured";
-                    newImage.height = imgWidth + '%';
-                    pnlMedia.addChild(newImage);
-                }
-            }
-
+            ReloadImages();
 
         }).catch(function (err) {
             console.log("Error -> " + err.message);
+        });
+}
+
+
+PostPage.prototype.OpenGallery = function () {
+    var context = imagepickerModule.create({
+        mode: "multiple"
+    });
+
+    context
+        .authorize()
+        .then(function () {
+            return context.present();
+        })
+        .then(function (selection) {
+            console.log("Selection done:");
+            selection.forEach(function (selected) {
+                console.log(" - " + selected.uri);
+                var newImageCaptured = { Id: generateGuid(), ImagePath: selected.fileUri, Thumb: selected.thumb };
+                selectedImages.push(newImageCaptured);
+
+                ReloadImages();
+            });
+        }).catch(function (e) {
+            console.log(e);
         });
 }
 
