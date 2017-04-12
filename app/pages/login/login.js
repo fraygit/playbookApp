2529cf83f542875286d58b9ecc27d4ccbe4ec6d1
fray <francis.yanga@gmail.com>
2017-04-12 22:44:27 +1200

    login page

--- app/pages/login/login.js
+++ app/pages/login/login.js
@@ -1,8 +1,9 @@
 ﻿var frameModule = require("ui/frame");
+var appSettings = require("application-settings");
+var http = require("http");
+var dialogs = require("ui/dialogs");
 
-exports.pageLoaded = function () {
-
-};
+var page;
 
 var navigationEntry = {
     moduleName: "pages/home/home",
@@ -14,7 +15,54 @@ var navigationEntry = {
     }
 };
 
+exports.pageLoaded = function (args) {
+    page = args.object;
+    console.log('app started - login page');
+    var token = appSettings.getString("token", "");
+
+    // Add token validation
+    if (!global.IsBlank(token)) {
+        frameModule.topmost().navigate(navigationEntry);
+    }
+};
+
+
+
 exports.Login = function () {
-    frameModule.topmost().navigate(navigationEntry);
+    txtUsername = page.getViewById("txtUsername");
+    txtPassword = page.getViewById("txtPassword");
+
+    console.log("login");
+
+    http.request({
+        url: global.ApiUrl + '/User',
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        content: JSON.stringify({ Email: txtUsername.text, Password: txtPassword.text })
+    }).then(function (response) {
+        console.log("response:" + response.statusCode);
+        if (response.statusCode == 200) {
+            var userToken = JSON.parse(response.content);
+            console.log("token:" + userToken.UserToken.Token);
+
+            appSettings.setString("token", userToken.UserToken.Token);
+
+            frameModule.topmost().navigate(navigationEntry);
+        }
+        else {
+            dialogs.alert("Invalid username or password!").then(function () {
+                return;
+            })
+        }
+
+    }, function (e) {
+        console.log("Error occurred " + e);
+        console.log("url:" + global.ApiUrl + '/PostStory');
+        dialogs.alert("Invalid username or password!").then(function () {
+            return;
+        })
+    });
+
+    
 }
 
